1b601ff77b7d53d2345a60f86cd5f411210dbcd9
Mo-AShraaf <74676105+Mohamed-Ashraf-2210@users.noreply.github.com>
2023-03-16 18:43:17 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
